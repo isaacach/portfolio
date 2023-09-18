fdf1b28b117bf8dc2a6c5f03e885387fa238cce9
isaacach <124728947+isaacach@users.noreply.github.com>
2023-09-18 10:52:18 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar.jsx'
+import './App.css'
+import './resets.css'
+import Aside from './components/Aside.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Aside />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="link-wrapper">
+        <li>About me</li>
+      </div>
+      <div className="link-wrapper">
+        <li>Projects</li>
+      </div>
+      <div className="link-wrapper">
+        <li>Contact</li>
+      </div>
+    </nav>
+  )
+}
